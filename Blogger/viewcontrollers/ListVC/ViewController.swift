8ebb259a7279484acfc30756ec1dcf7e3cdeae30
Prabhakar Patil <prabhakar@squats.in>
2020-10-24 12:29:22 +0530

    Project Structure Setup

--- Blogger/viewcontrollers/ListVC/ViewController.swift
+++ Blogger/viewcontrollers/ListVC/ViewController.swift
@@ -8,12 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
 }
 
